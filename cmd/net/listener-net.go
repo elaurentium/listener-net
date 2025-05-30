f55dc852c815735f4c2863944a3ca185ba070e4f
evandro.limeira <evandro.limeira@itadvanced.com.br>
2025-05-30 14:31:06 -0300

    hello world

---
+++ cmd/net/listener-net.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"elaurentium/listener-net/cmd"
+	"fmt"
+)
+
+func init() {
+	cmd.PrintBanner()
+}
+
+func main() {
+	fmt.Printf("Welcome to the Listener Net application!\n")
+}
